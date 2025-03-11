93c2a9a8178dc062339dcc1da31fe183f437c010
jay-tell3 <jaybroww@gmail.com>
2025-03-11 15:12:36 -0500

    ghjkgkgjk

--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("type a wholn number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number >= 0)
+            {
+                Console.WriteLine($"{number} is a positive number");
+            }
+            else 
+            {
+                Console.WriteLine($"{number} is a negitive number");
+            }
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is a positve number");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is a positve number");
+            }
         }
     }
 }
